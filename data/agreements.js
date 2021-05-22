676c4802e95c0dbc34da52bd0522d44c8ea8f42f
Paul Ccari <paulclindo@gmail.com>
2021-05-21 20:50:47 -0500

    add temp mock data for agreements

---
+++ data/agreements.js
@@ -0,0 +1,75 @@
+const agreements = [
+  {
+    id: 1,
+    title: 'Manejo de la pandemia con base científica',
+    description:
+      'Que juren poner en marcha en forma inmediata los máximos esfuerzos del Estado y otros existentes en el país para vencer la pandemia mediante estrategias eficaces con base científica para la prevención del contagio, la aplicación de tratamientos y la vacunación universal desarrollando el mayor empeño para enfrentar la trágica situación de pobreza y hambre causada por la pandemia.',
+  },
+  {
+    id: 2,
+    title: 'Respeto y defensa de los DDHH',
+    description:
+      'Que juren respetar y defender el derecho fundamental a la vida y garantizar los derechos humanos de todos los habitantes de la República, lo que implica respetar los derechos de sus minorías y los marginados para garantizar una sociedad inclusiva, tolerante y plural.',
+  },
+  {
+    id: 3,
+    title: 'Dejar el poder el 28 de julio del 2026',
+    description:
+      'Que juren dejar el poder después del 28 de julio de 2026 y no buscar ni intentar ningún mecanismo de reelección.',
+  },
+  {
+    id: 4,
+    title: 'Respetar la constitución',
+    description:
+      'Que juren que cualquier cambio y/o reforma, incluida la de la Constitución, solo se hará a través de los mecanismos constitucionales vigentes y en respeto al Estado de Derecho.',
+  },
+  {
+    id: 5,
+    title: 'Respetar las instituciones del Estado',
+    description:
+      'Que juren respetar y proteger la independencia y los fueros de los otros poderes del Estado',
+  },
+  {
+    id: 6,
+    title: 'Promover educación de calidad e igualdad',
+    description:
+      'Que juren promover una educación de calidad, con equidad e inclusión para nuestros estudiantes, aumentando la igualdad de oportunidades para niños, niñas, adolescentes y jóvenes, fortaleciendo además el trabajo de la SUNEDU.',
+  },
+  {
+    id: 7,
+    title: 'Respetar y defender la libertad de expresión',
+    description:
+      'Que juren respetar, estimular y defender decididamente la libertad de expresión y de prensa.',
+  },
+  {
+    id: 8,
+    title: 'Asegurar la libre asociación de ciudadanos',
+    description:
+      'Que juren asegurar la libre asociación en sindicatos, gremios, colegios profesionales y organizaciones no gubernamentales, vecinales y de base, entre otras formas de asociación ciudadana.',
+  },
+  {
+    id: 9,
+    title: 'Manejo de la pandemia con base científica',
+    description:
+      'Que juren poner en marcha en forma inmediata los máximos esfuerzos del Estado y otros existentes en el país para vencer la pandemia mediante estrategias eficaces con base científica para la prevención del contagio, la aplicación de tratamientos y la vacunación universal desarrollando el mayor empeño para enfrentar la trágica situación de pobreza y hambre causada por la pandemia.',
+  },
+  {
+    id: 10,
+    title: 'Manejo de la pandemia con base científica',
+    description:
+      'Que juren poner en marcha en forma inmediata los máximos esfuerzos del Estado y otros existentes en el país para vencer la pandemia mediante estrategias eficaces con base científica para la prevención del contagio, la aplicación de tratamientos y la vacunación universal desarrollando el mayor empeño para enfrentar la trágica situación de pobreza y hambre causada por la pandemia.',
+  },
+  {
+    id: 11,
+    title: 'Manejo de la pandemia con base científica',
+    description:
+      'Que juren poner en marcha en forma inmediata los máximos esfuerzos del Estado y otros existentes en el país para vencer la pandemia mediante estrategias eficaces con base científica para la prevención del contagio, la aplicación de tratamientos y la vacunación universal desarrollando el mayor empeño para enfrentar la trágica situación de pobreza y hambre causada por la pandemia.',
+  },
+  {
+    id: 12,
+    title: 'Manejo de la pandemia con base científica',
+    description:
+      'Que juren poner en marcha en forma inmediata los máximos esfuerzos del Estado y otros existentes en el país para vencer la pandemia mediante estrategias eficaces con base científica para la prevención del contagio, la aplicación de tratamientos y la vacunación universal desarrollando el mayor empeño para enfrentar la trágica situación de pobreza y hambre causada por la pandemia.',
+  },
+];
+export { agreements };
