8766b5718b02852af18f9597eb33f8a96a8a7989
Juan La Jara <juanlajara@gmail.com>
2021-05-22 01:28:21 -0500

    Add Signature count
    Co-authored-by: Luighi Viton-Zorrilla <luighiavz@gmail.com>

---
+++ components/SignatureCount/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SignatureCount = styled('p')`
+  border-bottom: 1px solid #d9d9d9;
+  color: #4e4a49;
+  font-size: 2.5rem;
+  font-weight: bold;
+  line-height: 3.625rem;
+  margin: 0;
+  padding: 0 4.9375rem 2rem;
+`;
