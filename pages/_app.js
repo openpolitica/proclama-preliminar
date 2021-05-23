d6364903a3740184b51bc474d617c34e345d9813
Juan La Jara <juanlajara@gmail.com>
2021-05-23 15:18:33 -0500

    Add Google analytics support
    Following https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
    example.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import { ThemeProvider } from 'styled-components';
 import theme from 'global-styles/theme';
 import GlobalStyle from 'global-styles/global';
 import Head from 'next/head';
+import * as gtag from 'lib/gtag';
 
 function MyOPApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = url => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -31,5 +46,10 @@ function MyOPApp({ Component, pageProps }) {
     </>
   );
 }
+
+MyOPApp.propTypes = {
+  Component: PropTypes.func,
+  pageProps: PropTypes.object,
+};
 
 export default MyOPApp;
