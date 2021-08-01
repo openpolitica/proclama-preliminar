b91e735665b20d7a2605c1ed5503cc6272d2f93f
Luighi <luighiavz@gmail.com>
2021-08-01 13:17:49 -0500

    Update background and header color

--- components/Home/styles.js
+++ components/Home/styles.js
@@ -8,7 +8,8 @@ export const Container = styled('div')`
 `;
 
 export const Header = styled('h1')`
-  border-bottom: 1px solid #c4c4c4;
+  background-color: rgba(255, 255, 255, 0.6);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.06);
   color: #4e4a49;
   font-size: 1.375rem;
   font-weight: bold;
