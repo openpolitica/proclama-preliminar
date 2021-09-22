a1c2e9f073cac7de159b27f35fabf591c80e128a
Luighi <luighiavz@gmail.com>
2021-09-21 20:40:35 -0500

    Add sort to agreements request

--- pages/api/agreements/index.js
+++ pages/api/agreements/index.js
@@ -3,12 +3,12 @@ import AgreementsSchema from 'models/Agreements';
 
 export const getAgreementsOnly = async () => {
   await connectToDatabase();
-  return await AgreementsSchema.find({});
+  return await AgreementsSchema.find({}).sort({ id: 1 });
 };
 
 export const getAgreements = async () => {
   await connectToDatabase();
-  return await AgreementsSchema.find({}).populate('events');
+  return await AgreementsSchema.find({}).sort({ id: 1 }).populate('events');
 };
 
 export default async (req, res) => {
@@ -19,7 +19,9 @@ export default async (req, res) => {
   switch (method) {
     case 'GET':
       try {
-        const agreements = await AgreementsSchema.find({}).populate('events');
+        const agreements = await AgreementsSchema.find({})
+          .sort({ id: 1 })
+          .populate('events');
         res.status(200).json({ success: true, data: agreements });
       } catch (error) {
         res.status(400).json({ success: false, message: error.toString() });
