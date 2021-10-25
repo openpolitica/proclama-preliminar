7ef318b760cfee2b740c5a6bc2020b1278346c75
Luighi <luighiavz@gmail.com>
2021-10-24 23:50:18 -0500

    Delete dynamic counter in signatures

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import Home from 'components/Home';
-import { getSignatureCount } from 'pages/api/signatures';
 import { getAgreements } from 'pages/api/agreements';
 import { getEvents } from 'pages/api/events';
 
 export async function getServerSideProps() {
-  const signatures = await getSignatureCount();
   const agreements = await getAgreements();
   const events = await getEvents();
   return {
     props: {
-      signatures: signatures,
       agreements: JSON.parse(JSON.stringify(agreements)),
       events: JSON.parse(JSON.stringify(events.reverse())),
     },
