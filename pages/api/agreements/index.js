67288135ff8347bc4f5802c000165707d1db3941
Luighi <luighiavz@gmail.com>
2022-02-15 00:16:12 -0500

    Add funtion to count status

--- pages/api/agreements/index.js
+++ pages/api/agreements/index.js
@@ -2,6 +2,7 @@ import { connectToDatabase } from 'util/mongodb';
 import 'models/Indicators'; //required to populate indicators
 import 'models/Events'; //required to populate events
 import AgreementsSchema from 'models/Agreements';
+import IndicatorsSchema from 'models/Indicators';
 
 export const getAgreementsOnly = async () => {
   await connectToDatabase();
@@ -26,6 +27,106 @@ export const getAgreementByID = async id => {
       path: 'events',
     },
   });
+};
+
+export const getStatusCounters = async () => {
+  await connectToDatabase();
+  return await IndicatorsSchema.aggregate([
+    {
+      $lookup: {
+        from: 'events',
+        localField: 'indicator_id',
+        foreignField: 'indicator_id',
+        as: 'events',
+      },
+    },
+    {
+      $unwind: {
+        path: '$events',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $replaceRoot: {
+        newRoot: {
+          $mergeObjects: ['$events', '$$ROOT'],
+        },
+      },
+    },
+    {
+      $project: {
+        events: 0,
+        title: 0,
+        description: 0,
+        data_source: 0,
+        data_source_url: 0,
+      },
+    },
+    {
+      $sort: { created_At: 1, indicator_id: 1 },
+    },
+    {
+      $group: {
+        _id: '$_id',
+        last_status: { $last: '$status' },
+        indicator_id: { $last: '$indicator_id' },
+        agreement_id: { $last: '$agreement_id' },
+      },
+    },
+    {
+      $group: {
+        _id: '$agreement_id',
+        indicators_count: { $sum: 1 },
+        statuses: { $push: '$last_status' },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        indicators_count: 1,
+        moderate: {
+          $size: {
+            $filter: {
+              input: '$statuses',
+              as: 'item',
+              cond: { $eq: ['$$item', 'moderate'] },
+            },
+          },
+        },
+        high: {
+          $size: {
+            $filter: {
+              input: '$statuses',
+              as: 'item',
+              cond: { $eq: ['$$item', 'high'] },
+            },
+          },
+        },
+        extreme: {
+          $size: {
+            $filter: {
+              input: '$statuses',
+              as: 'item',
+              cond: { $eq: ['$$item', 'extreme'] },
+            },
+          },
+        },
+      },
+    },
+    {
+      $set: {
+        null: {
+          $subtract: [
+            '$indicators_count',
+            { $add: ['$moderate', '$high', '$extreme'] },
+          ],
+        },
+      },
+    },
+    {
+      $sort: { _id: 1 },
+    },
+  ]);
 };
 
 export default async (req, res) => {
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Home from 'components/Home';
-import { getAgreements } from 'pages/api/agreements';
+import { getAgreements, getStatusCounters } from 'pages/api/agreements';
 import { getLastEvents } from 'pages/api/events';
 
 export async function getServerSideProps() {
   const agreements = await getAgreements();
   const events = await getLastEvents(3);
-  console.log(events);
+  const counters = await getStatusCounters();
+  console.log(counters);
   return {
     props: {
       agreements: JSON.parse(JSON.stringify(agreements)),
       events: JSON.parse(JSON.stringify(events)),
+      counters: JSON.parse(JSON.stringify(counters)),
     },
   };
 }
