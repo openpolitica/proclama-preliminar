fe9590ed865c248a803f00f4535113acf461a805
Luighi <luighiavz@gmail.com>
2022-02-15 13:53:59 -0500

    Combine info from counter and agreements
    Uses static for status order, next version will have this data in the
    database to check the order for each status

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,56 @@ export async function getServerSideProps() {
   const agreements = await getAgreements();
   const events = await getLastEvents(3);
   const counters = await getStatusCounters();
-  console.log(counters);
+
+  const status = [
+    {
+      id: 'null',
+      order: 0,
+    },
+    {
+      id: 'moderate',
+      order: 1,
+    },
+    {
+      id: 'high',
+      order: 2,
+    },
+    {
+      id: 'extreme',
+      order: 3,
+    },
+  ];
+
+  const agreementsCounters = agreements.map(
+    ({ id, title, description, _id }) => {
+      const agreementCounters = counters.find(item => item._id === id);
+      agreementCounters.statuses = agreementCounters.statuses.sort((a, b) => {
+        const aStatus = status.find(item => item.id === a.status);
+        const bStatus = status.find(item => item.id === b.status);
+
+        if (aStatus?.order > bStatus?.order) {
+          return 1;
+        }
+        if (aStatus?.order < bStatus?.order) {
+          return -1;
+        }
+        return 0;
+      });
+
+      return {
+        _id,
+        id,
+        title,
+        description,
+        statuses: agreementCounters.statuses,
+        indicators_count: agreementCounters.indicators_count,
+      };
+    },
+  );
+  console.log(agreementsCounters);
   return {
     props: {
-      agreements: JSON.parse(JSON.stringify(agreements)),
+      agreements: JSON.parse(JSON.stringify(agreementsCounters)),
       events: JSON.parse(JSON.stringify(events)),
       counters: JSON.parse(JSON.stringify(counters)),
     },
