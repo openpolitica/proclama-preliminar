18f0a9bcfce62d42e4e20209fdaf8040d8fad106
Luighi <luighiavz@gmail.com>
2022-08-04 17:55:30 -0500

    Add sponsor to footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,13 +3,25 @@ import * as Styled from './styles';
 const Footer = () => {
   return (
     <Styled.Footer>
-      Desarrollado por{' '}
-      <Styled.TextLink
-        href="https://openpolitica.com/"
-        target="_blank"
-        rel="noopener noreferrer">
-        <Styled.OPLogo />
-      </Styled.TextLink>
+      <Styled.Container>
+        Con el apoyo de{' '}
+        <Styled.TextLink
+          href="https://www.brot-fuer-die-welt.de/es/pan-para-el-mundo/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <Styled.BrotLogo />
+        </Styled.TextLink>
+      </Styled.Container>
+      <Styled.Separator />
+      <Styled.Container>
+        Desarrollado por{' '}
+        <Styled.TextLink
+          href="https://openpolitica.com/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <Styled.OPLogo />
+        </Styled.TextLink>
+      </Styled.Container>
     </Styled.Footer>
   );
 };
